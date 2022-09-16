fec3301442bc3edda5216c410fb3cf84662488f6
sakanal <sakanal9527@gmail.com>
2022-09-16 14:31:23 +0800

    添加用户搜索实体类

--- src/main/java/com/example/mall/service/UserInfoService.java
+++ src/main/java/com/example/mall/service/UserInfoService.java
@@ -3,11 +3,12 @@ package com.example.mall.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.mall.entity.UserInfoEntity;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.mall.vo.UserInfoQuery;
 
 import java.util.Map;
 
 /**
- * 
+ *
  *
  * @author ouyang
  * @email wanbzoy@163.com
@@ -15,5 +16,7 @@ import java.util.Map;
 public interface UserInfoService extends IService<UserInfoEntity> {
 
 	Page<UserInfoEntity> getPage(Map<String, Object> params);
+
+    Page<UserInfoEntity> getPage(Integer current, UserInfoQuery userInfoQuery);
 }
 
