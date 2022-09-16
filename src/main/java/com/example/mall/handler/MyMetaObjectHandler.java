85a433ffcd0c9a69556b15ea26930acaae0817c1
sakanal <sakanal9527@gmail.com>
2022-09-16 16:29:44 +0800

    添加自动填充过滤器

---
+++ src/main/java/com/example/mall/handler/MyMetaObjectHandler.java
@@ -0,0 +1,26 @@
+package com.example.mall.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        this.strictInsertFill(metaObject,"create_time", LocalDateTime.class,LocalDateTime.now());
+
+        this.strictUpdateFill(metaObject,"update_time", LocalDateTime.class,LocalDateTime.now());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.strictUpdateFill(metaObject,"update_time", LocalDateTime.class,LocalDateTime.now());
+    }
+}
