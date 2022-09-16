366a3582d2e363fa347c09ae158f7cf30a80b318
sakanal <sakanal9527@gmail.com>
2022-09-16 11:12:45 +0800

    解决跨域

--- src/main/java/com/example/mall/config/CorsConfig.java
+++ src/main/java/com/example/mall/config/CorsConfig.java
@@ -23,8 +23,8 @@ public class CorsConfig {
         final CorsConfiguration corsConfiguration = new CorsConfiguration();
         /*是否允许请求带有验证信息*/
         corsConfiguration.setAllowCredentials(true);
-        /*允许访问的客户端域名*/
-        corsConfiguration.addAllowedOrigin("*");
+//        /*允许访问的客户端域名*/
+        corsConfiguration.addAllowedOriginPattern("*");
         /*允许服务端访问的客户端请求头*/
         corsConfiguration.addAllowedHeader("*");
         /*允许访问的方法名,GET POST等*/
