7d037d856b024e6b2823544cf3256b300baffe54
sakanal <sakanal9527@gmail.com>
2022-09-16 10:52:42 +0800

    添加swagger

--- src/main/java/com/example/mall/config/SwaggerConfig.java
+++ src/main/java/com/example/mall/config/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.example.mall.config;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -18,6 +19,7 @@ public class SwaggerConfig {
                 .groupName("webApi")
                 .apiInfo(webApiInfo())
                 .select()
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
                 .build();
     }
 
