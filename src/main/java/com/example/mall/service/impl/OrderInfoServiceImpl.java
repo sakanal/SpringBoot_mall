ec4e29fd29c8f94779fbb7a05969e80db1bbba17
ouyang <2501947461@qq.com>
2022-09-19 16:44:45 +0800

    完成保存订单信息

--- src/main/java/com/example/mall/service/impl/OrderInfoServiceImpl.java
+++ src/main/java/com/example/mall/service/impl/OrderInfoServiceImpl.java
@@ -1,17 +1,21 @@
 package com.example.mall.service.impl;
 
 import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.toolkit.IdWorker;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.mall.constant.SelectArg;
 import com.example.mall.entity.OrderProductEntity;
 import com.example.mall.entity.ProductInfoEntity;
 import com.example.mall.service.OrderProductService;
 import com.example.mall.service.ProductInfoService;
+import com.example.mall.vo.OrderInfoVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -20,6 +24,7 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.example.mall.dao.OrderInfoDao;
 import com.example.mall.entity.OrderInfoEntity;
 import com.example.mall.service.OrderInfoService;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Service("orderInfoService")
@@ -59,6 +64,34 @@ public class OrderInfoServiceImpl extends ServiceImpl<OrderInfoDao, OrderInfoEnt
 	@Override
 	public List<OrderInfoEntity> getProductsByUserId(String id) {
 		return this.list(new QueryWrapper<OrderInfoEntity>().eq(StrUtil.isBlank(id),"user_id",id));
+	}
+
+	@Transactional
+	@Override
+	public void saveOrder(List<OrderInfoVo> orderInfoList) {
+		String uuid = IdWorker.get32UUID();
+		OrderInfoEntity orderInfoEntity = new OrderInfoEntity();
+		orderInfoEntity.setUserId(orderInfoList.get(0).getUserId());
+		Double total = new Double(0d);
+		orderInfoEntity.setOrderSn(uuid);
+		for (OrderInfoVo orderInfoVo : orderInfoList) {
+			OrderProductEntity orderProductEntity = new OrderProductEntity();
+			orderProductEntity.setOrderSn(uuid);
+			orderProductEntity.setNumber(orderInfoVo.getProductNumber());
+			orderProductEntity.setProductId(orderInfoVo.getProductId());
+			ProductInfoEntity productInfo = productInfoService.getById(orderInfoVo.getProductId());
+			if (productInfo!=null){
+				total+=productInfo.getPrice()*orderInfoVo.getProductNumber();
+			}
+			orderProductService.save(orderProductEntity);
+		}
+		orderInfoEntity.setAddressId(orderInfoList.get(0).getAddressId());
+		orderInfoEntity.setCreateTime(new Date());
+		Double freightAmount= new Random().nextDouble()*100;
+		orderInfoEntity.setFreightAmount(freightAmount);
+		orderInfoEntity.setTotalAmount(total);
+		orderInfoEntity.setPayAmount(total+freightAmount);
+		this.save(orderInfoEntity);
 	}
 
 	private Integer getOrderProductNumber(String id, List<OrderProductEntity> orderProdectList) {
