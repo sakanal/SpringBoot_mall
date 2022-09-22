7529604602be73a8246fa40e04649b63d8df1f18
sakanal <sakanal9527@gmail.com>
2022-09-22 13:55:54 +0800

    设置默认产品图片

---
+++ src/main/java/com/example/mall/constant/DefaultPicture.java
@@ -0,0 +1,10 @@
+package com.example.mall.constant;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class DefaultPicture {
+    public final static String DEFAULT_URL = "[{\"url\": \"https://mall-sakanal.oss-cn-hangzhou.aliyuncs.com/2022/09/22/bf4dda1504de431980a75a501136f722默认图片.png\", \"name\": \"默认图片.png\"}]";
+}
--- src/main/java/com/example/mall/controller/ProductInfoController.java
+++ src/main/java/com/example/mall/controller/ProductInfoController.java
@@ -8,6 +8,7 @@ import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.mall.constant.DefaultPicture;
 import com.example.mall.constant.SelectArg;
 import com.example.mall.vo.PictureVo;
 import com.example.mall.entity.ProductInfoEntity;
@@ -121,6 +122,8 @@ public class ProductInfoController {
 		if (pictureList.size() > 0) {
 			String str = JSONUtil.toJsonStr(pictureList);
 			productInfo.setPicture(str);
+		}else {
+			productInfo.setPicture(DefaultPicture.DEFAULT_URL);
 		}
 		productInfoService.save(productInfo);
 
